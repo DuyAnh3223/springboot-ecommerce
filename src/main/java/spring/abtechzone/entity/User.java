d0f98dbc87fd4acd2cf47628bac158cb3b3a9892
Duy Anh <duyanh3203@gmail.com>
2026-06-16 20:49:21 +0700

    add unique constraint for createUser at database level

--- src/main/java/spring/abtechzone/entity/User.java
+++ src/main/java/spring/abtechzone/entity/User.java
@@ -20,12 +20,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     String id;
 
+    @Column(name = "username" ,unique = true, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
     String username;
     String password;
     String firstName;
     String lastName;
-    //     boolean delFlag;
-    //     boolean status;
 
     @ManyToMany
     Set<Role> roles; // Các phần tử trong Set là unique
