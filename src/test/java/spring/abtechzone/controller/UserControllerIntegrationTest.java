c293ca93a58f6e42d1ab0afe1a9afe10c7f85f61
Duy Anh <duyanh3203@gmail.com>
2026-06-16 15:02:23 +0700

    add integration test & Code coverage with  Jacoco:Report

--- src/test/java/spring/abtechzone/controller/UserControllerIntegrationTest.java
+++ src/test/java/spring/abtechzone/controller/UserControllerIntegrationTest.java
@@ -7,30 +7,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
 import spring.abtechzone.dto.request.UserCreationRequest;
 import spring.abtechzone.dto.response.UserResponse;
-import spring.abtechzone.service.UserService;
 import tools.jackson.databind.ObjectMapper;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource("/test.properties")
-public class UserControllerTest {
+@Testcontainers
+public class UserControllerIntegrationTest {
+    @Container
+    static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer("mysql:latest");
+
+
+//    Connect test database on docker
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driver-class-name", ()->"com.mysql.cj.jdbc.Driver");
+        registry.add("spring.jpa.hibernate.ddl-auto", ()->"update");
+
+    }
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockitoBean
-    private UserService userService;
 
     private UserCreationRequest request;
     private UserResponse response;
@@ -59,44 +72,23 @@ public class UserControllerTest {
         ObjectMapper mapper = new ObjectMapper();
         String content = mapper.writeValueAsString(request);
 
-        when(userService.createUser(any())).thenReturn(response);
+//        when(userService.createUser(any())).thenReturn(response);
 
         // WHEN
-        mockMvc.perform(post("/users")
+        var response = mockMvc.perform(post("/users")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(content))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code")
-                        .value(1000))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.result.id")
-                        .value("dwqsadsadg563433")
-        );
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.username").value("username"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.firstName").value("firstName"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.lastName").value("lastName"));
+
+        log.info("Result: {}",response.andReturn().getResponse().getContentAsString());
 
         // THEN
     }
 
-    @Test
-        //
-    void createUser_usernameInvalid_fail() throws Exception {
-        // GIVEN
-        request.setUsername("us");
-        ObjectMapper mapper = new ObjectMapper();
-        String content = mapper.writeValueAsString(request);
 
-//        Mockito.when(userService.createUser(any())).thenReturn(response);
-
-        // WHEN
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(content))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code")
-                        .value(1003))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message")
-                        .value("Username must be at least 3 characters")
-                );
-
-        // THEN
-    }
 
 }
--- src/test/java/spring/abtechzone/service/UserServiceTest.java
+++ src/test/java/spring/abtechzone/service/UserServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import spring.abtechzone.dto.request.UserCreationRequest;
 import spring.abtechzone.dto.response.UserResponse;
@@ -12,6 +14,8 @@ import spring.abtechzone.exception.AppException;
 import spring.abtechzone.repository.UserRepository;
 
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -19,6 +23,7 @@ import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestPropertySource("/test.properties")
 public class UserServiceTest {
 
     @Autowired
@@ -81,6 +86,28 @@ public class UserServiceTest {
         var exception = assertThrows(AppException.class, () -> userService.createUser(request));
 
         assertThat(exception.getErrorCode().getCode()).isEqualTo(1001);
+    }
+
+    @Test
+    @WithMockUser(username = "username" )
+    void getMyInfo_valid_success(){
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user));
+
+        var response = userService.getMyInfo();
+
+        assertThat(response.getId()).isEqualTo("dwqsadsadg563433");
+        assertThat(response.getUsername()).isEqualTo("username");
+    }
+
+    @Test
+    @WithMockUser(username = "username" )
+    void getMyInfo_userNotFound_error(){
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.ofNullable(null));
+
+        var exception = assertThrows(AppException.class,
+                () -> userService.getMyInfo());
+
+        assertThat(exception.getErrorCode().getCode()).isEqualTo(1002);
     }
 
 
