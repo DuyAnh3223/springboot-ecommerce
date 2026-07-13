d15672651d442cc633a96b07453cf8a7f821ff3d
Duy Anh <duyanh3203@gmail.com>
2026-07-13 19:42:42 +0700

    fix(server): modify to apply docker compose

--- server/src/main/java/spring/abtechzone/common/config/RedisConfig.java
+++ server/src/main/java/spring/abtechzone/common/config/RedisConfig.java
@@ -3,17 +3,22 @@ package spring.abtechzone.common.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${REDIS_ADDRESS:redis://127.0.0.1:3308}")
+    private String redisAddress;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         // kết nối tới redis server
         config.useSingleServer()
-                .setAddress("redis://127.0.0.1:3308")
+                .setAddress(redisAddress)
                 .setConnectionPoolSize(10)
                 .setConnectionMinimumIdleSize(5)
                 .setConnectTimeout(30000);
