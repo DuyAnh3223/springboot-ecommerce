f0a9415238e8b8893322314f07580f6c19acf4c7
Duy Anh <duyanh3203@gmail.com>
2026-08-08 13:17:22 +0700

    feat(cart): add mergeGuestCart action and integrate with CartInitializer

--- client/features/customer/cart/components/CartInitializer.tsx
+++ client/features/customer/cart/components/CartInitializer.tsx
@@ -3,6 +3,8 @@
 import { createContext, useContext, useEffect, useSyncExternalStore } from "react";
 import { CartSnapshot } from "../types/cart.types";
 import { useCartStore } from "../stores/cart.store";
+import { getGuestCart, clearGuestCart } from "../utils/guest-cart.utils";
+import { mergeGuestCartAction } from "../actions/merge-guest-cart.actions";
 
 interface CartHydrationContextValue {
   hydrationKey: string;
@@ -45,8 +47,32 @@ export function CartInitializer({ cart, scopeKey, children }: CartInitializerPro
   useEffect(() => {
     if (cart) {
       useCartStore.getState().setCart(cart, hydrationKey);
+
+      const guestItems = getGuestCart();
+      if (guestItems.length > 0) {
+        mergeGuestCartAction(
+          guestItems.map((i) => ({ productSkuId: i.productSkuId, quantity: i.quantity }))
+        )
+          .then((res) => {
+            if (res.success && res.data) {
+              useCartStore.getState().setCart(res.data, hydrationKey);
+            }
+            clearGuestCart();
+          })
+          .catch(() => {
+            clearGuestCart();
+          });
+      }
     } else {
-      useCartStore.getState().reset(hydrationKey);
+      const guestItems = getGuestCart();
+      const guestSnapshot: CartSnapshot = {
+        cartId: null,
+        status: "ACTIVE",
+        items: guestItems,
+        userId: null,
+        isGuest: true,
+      };
+      useCartStore.getState().setCart(guestSnapshot, hydrationKey);
     }
   }, [cart, hydrationKey]);
 
